59e2e20bbe2e2e51c420f2899048c3a7ff44352f
Poojahiremath983 <poojahiremath983@gmail.com>
2020-06-14 16:00:57 +0530

    first commit

---
+++ inventory/admin.py
@@ -0,0 +1,11 @@
+
+# Register your models here.
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import *
+# Register your models here.
+
+# admin.site.register(item)
+@admin.register(Desktops, Laptops, Mobiles)
+class ViewAdmin(ImportExportModelAdmin):
+    exclude = ('id', )
